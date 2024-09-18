f7fa1177788ae80d166bcf80cd6a0f76adee0311
taaaf11 <taafuuu@gmail.com>
2024-09-18 19:15:06 +0500

    Fix buggy code.

--- src/arxd/arxd.py
+++ src/arxd/arxd.py
@@ -1,4 +1,5 @@
 """
+Name: arxd
 Author: Muhammad Altaaf
 Contact: taafuuu@gmail.com
 Description: A script to extract (and delete if asked) all the archive files present in cwd.
@@ -8,8 +9,12 @@ Description: A script to extract (and delete if asked) all the archive files pre
 import os
 import shutil
 import sys
+import typing
 from argparse import ArgumentParser
 from dataclasses import dataclass
+
+if typing.TYPE_CHECKING:
+    from collections.abc import Iterable
 
 # a prefix is made up of two parts
 # one part: the parent directory (specified with -p cli option)
@@ -26,34 +31,30 @@ class _Config:
     auto_del: bool = False
 
 
-def get_avail_ar_fmts() -> list[str]:
-    """Returns list of available archive formats."""
+def get_avail_ar_exts() -> Iterable[str]:
+    """Returns iterable of available archive formats."""
 
-    fmts = []
-    fmts_detail = shutil.get_unpack_formats()
-    for fmt_detail in fmts_detail:
-        fmts.extend(fmt_detail[1])
-    return fmts
+    for fmt_name, fmt_ext, fmt_desc in shutil.get_unpack_formats():
+        yield from fmt_ext
 
 
 def is_ar(filename: str) -> bool:
     """Returns whether given filename is name of an archive."""
 
-    is_file_ar = False
-    avail_ar_fmts = get_avail_ar_fmts()
-    for fmt in avail_ar_fmts:
-        if filename.endswith(filename):
+    for fmt in get_avail_ar_exts():
+        if filename.endswith(fmt):
             return True
     return False
 
 
 def split_ext(filename: str) -> bool:
-    """Split archive filename into two parts: filename without extension, the extension."""
+    """Split archive filename into two parts: filename without
+    extension, the extension. Don't confuse it with os.path.splitext.
+    """
 
-    avail_ar_fmts = get_avail_ar_fmts()
-    for fmt in avail_ar_fmts:
+    for fmt in get_avail_ar_exts():
         if filename.endswith(fmt):
-            return filename.split(fmt)[0]
+            return filename.removesuffix(fmt)
 
 
 def create_missing_dirs(prefix: str):
@@ -84,7 +85,7 @@ def create_missing_dirs(prefix: str):
 def ex_ar(filename: str, prefix: str | None = None) -> bool:
     """Extract archive file represented by filename."""
 
-    ex_dir = split_ext(filename)
+    ex_dir: str = split_ext(filename)
     if prefix:
         prefix = os.path.join(prefix, ex_dir)
     else:
@@ -95,33 +96,39 @@ def ex_ar(filename: str, prefix: str | None = None) -> bool:
     shutil.unpack_archive(filename, prefix)
 
 
-def extract_archives(filenames: str, config: _Config):
+def extract_archives(filenames: Iterable[str], prefix: str, auto_del: bool):
     """Wrapper function for ex_ar function."""
 
     for filename in filenames:
-        ex_ar(filename, config.prefix)
-        if config.auto_del:
+        ex_ar(filename, prefix)
+        if auto_del:
             os.remove(filename)
 
 
-def main():
-    filenames = os.listdir()
-    filenames = list(filter(lambda filename: not os.path.isdir(filename), filenames))
+def filter_ar(filename: str) -> bool:
+    """Returns whether given file name is of archive file.
+    """
 
-    # filter out filenames represeting archives
-    filenames = list(filter(is_ar, filenames))
+    return os.path.isfile(filename) and is_ar(filename)
+
+
+def main():
+    # get filenames of archives under pwd
+    filenames = filter(lambda filename: filter_ar(filename), os.listdir())
 
     parser = ArgumentParser()
     parser.add_argument(
         "-e",
         "--extract",
         action="store_true",
+        default=False,
         help="Start extraction. None of the options have effect without this option.",
     )
     parser.add_argument(
         "-d",
         "--delete",
         action="store_true",
+        default=False,
         help="Delete files after extraction is completed.",
     )
     parser.add_argument(
@@ -145,8 +152,7 @@ def main():
     if args.prefix:
         prefix = args.prefix.strip("/")
     if args.extract:
-        config = _Config(prefix, auto_del)
-        extract_archives(filenames, config)
+        extract_archives(filenames, prefix, auto_del)
 
 
 if __name__ == "__main__":
