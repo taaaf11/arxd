e85c5cb0a871d627955e9ffdca89a08f1b416012
taaaf11 <taafuuu@gmail.com>
2024-09-30 14:12:40 +0500

    Add Final type qualifier to constants

--- src/arxd/constants.py
+++ src/arxd/constants.py
@@ -1,21 +1,28 @@
-PROG_NAME = "arxd"
-PROG_NAME_DESIGN = """\
+from __future__ import annotations
+
+import typing
+
+if typing.TYPE_CHECKING:
+    from typing import Final
+
+PROG_NAME: Final = "arxd"
+PROG_NAME_DESIGN: Final = """\
 ┌─┐┬─┐─┐ ┬┌┬┐
 ├─┤├┬┘┌┴┬┘ ││
 ┴ ┴┴└─┴ └──┴┘\
 """
 
-PROG_VER = "2.0.0"
+PROG_VER: Final = "2.0.0"
 
-PROG_DESC = f"""\
+PROG_DESC: Final = f"""\
 {PROG_NAME_DESIGN}
 
 Extract (and delete) archive files under present working directory.\
 """
 
-PROG_AUTHOR = "Muhammad Altaaf <taafuuu@gmail.com>"
+PROG_AUTHOR: Final = "Muhammad Altaaf <taafuuu@gmail.com>"
 
-PROG_VER_INFO = f"""\
+PROG_VER_INFO: Final = f"""\
 {PROG_NAME_DESIGN}
 
 {PROG_NAME} version: {PROG_VER}.
